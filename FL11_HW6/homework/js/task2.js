270542f9501609b944cfcd9217c9cac164fc9647
FyuroTEX <yurilev4@gmail.com>
2019-07-05 19:13:02 +0300

    hw6v2

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -3,9 +3,7 @@ a = +prompt('A');
 b = +prompt('B');
 c = +prompt('C');
 p = (a + b + c) / 2;
-console.log(p);
 s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-console.log(s);
 if (isNaN(s) || a <= 0 || b <= 0 || c <= 0 || s == 0) {
   console.log('Triangle doesn’t exist');
 } else if (a == b && b == c && c == a) {
