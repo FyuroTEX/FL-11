c5773fbe67087b21fd8bc9f7d1424ceb296c8d30
FyuroTEX <yurilev4@gmail.com>
2019-07-07 15:32:15 +0300

    hw5v2.1

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -6,9 +6,9 @@ p = (a + b + c) / 2;
 s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 if (isNaN(s) || a <= 0 || b <= 0 || c <= 0 || s == 0) {
   console.log('Triangle doesn’t exist');
-} else if (a == b && b == c && c == a) {
+} else if (a === b && b === c && c === a) {
   console.log('Eequivalent triangle');
-} else if (a == b || b == c || c == a) {
+} else if (a === b || b === c || c === a) {
   console.log('Isosceles triangle');
 } else {
   console.log('Normal triangle');
