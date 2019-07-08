2d557a8fac343484c1fb2f7cacfe30d3421c8415
FyuroTEX <yurilev4@gmail.com>
2019-07-08 19:13:42 +0300

    add hw7

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,55 +1,59 @@
-let oldPassword, newPassword, password;
-let email = prompt('ENTER EMAIL');
-let elength = 6;
+let password, agenNewPass, newPassword, oldPassword;
+const minLength = 6;
+let email = prompt('ENTER EMAIL', '');
 let login = {
-  admin: {
-    email: 'admin@gmail.com',
-    password: 'AdminPass'
-  },
-  user: {
-    email: 'user@gmail.com',
-    password: 'UserPass'
-  }
+    admin: {
+        email: 'admin@gmail.com',
+        password: 'AdminPass'
+    },
+    user: {
+        email: 'user@gmail.com',
+        password: 'UserPass'
+    }
 };
-//email
 switch (true) {
-  case email === login.admin.email:
-  case email === login.user.email:
-    password = prompt('ENTER PASSWORD');
-    break;
-  case email !== null && email.length < elength && email.length !== 0:
-    alert("I don't know any emails having name length less than 6 symbols");
-    break;
-  case email !== null && email.length >= elength:
-    alert('I don’t know you');
-    break;
-  case null:
-  default:
-    alert('Canceled.');
-    break;
+    case email === login.admin.email:
+    case email === login.user.email:
+        password = prompt('ENTER PASSWORD', '');
+        break;
+    case email !== null && email.length < minLength && email.length !== 0:
+        alert('I don\'t know any emails having name length less than 6 symbols');
+        break;
+    case email !== null && email.length >= minLength:
+        alert('I don’t know you');
+        break;
+    case null:
+    default:
+        alert('Canceled.');
+        break;
 }
-//password
 switch (password) {
-  case login.admin.password:
-  case login.user.password:
-    confirm('Do you want to change your password?')
-      ? oldPassword = prompt('Write the old password')
-      : alert('You have failed the change.');
-    break;
-  case password || !null:
-    alert('Wrong password');
-    break;
-  case null:
-  default:
-    alert('Canceled.');
-    break;
+    case login.admin.password:
+    case login.user.password:
+        confirm('Do you want to change your password?')
+            ? oldPassword = prompt('Write the old password', '')
+            : alert('You have failed the change.');
+        break;
+    case password || !null:
+        alert('Wrong password');
+        break;
+    case null:
+    default:
+        alert('Canceled.');
+        break;
 }
-//write new password
-switch (oldPassword) {
-  case password:
-    newPassword = prompt('Write the new password');
-    break;
-  default:
+if (oldPassword === password && oldPassword.length >= minLength) {
+    newPassword = prompt('Write the new password', '');
+} else {
     alert('Canceled.');
-    break;
+}
+if (newPassword.length < minLength) {
+    alert('It’s too short password. Sorry.');
+} else if (newPassword.length >= minLength) {
+    agenNewPass = prompt('Enter password agen', '');
+}
+if (agenNewPass === newPassword && agenNewPass.length >= minLength) {
+    alert('You have successfully changed your password.');
+} else {
+    alert('You wrote the wrong password.');
 }
--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,54 @@
-// Your code goes here
+let randomNumber, playerNumber;
+let prize = 0; let attempt = 0; let two = 2; let maxAttempt = 3;
+let addPoket = 4; let gamePoket = 8; let resetPoket = 8;
+let maxPrize = 100; let midPrize = 50; let lowPrize = 25;
+let attemptPrize = [maxPrize, midPrize, lowPrize];
+let attemptLeft = [maxAttempt, two, 1];
+let newGame = true;
+let playGame = confirm('Do you want to play a game?');
+if (playGame) {
+    while (attempt <= maxAttempt) {
+        switch (true) {
+            case attempt === 0:
+                randomNumber = Math.floor(Math.random() * (gamePoket + 1));
+                console.log(randomNumber);
+                playerNumber = +prompt('Choose a roulette pocket number from 0 to ' + gamePoket +
+                    '\n Attempts left: ' + attemptLeft[attempt] +
+                    '\n Tottal prize: ' + prize +
+                    '$ \n Possible prize on current attempt ' + attemptPrize[attempt] + '$', '');
+                attempt++;
+                break;
+            case randomNumber === playerNumber:
+                prize += attemptPrize[attempt - 1];
+                gamePoket += addPoket;
+                attempt = 0;
+                alert('Congratulation, you won!   Your prize is: '
+                    + prize + '$.');
+                confirm('Do you want to continue?') ? playGame : attempt = maxAttempt + 1;
+                attemptPrize = attemptPrize.map(p => p * two);
+                break;
+            case attempt === maxAttempt:
+                alert('Thank you for your participation. Your prize is: ' + prize + '$');
+                prize = 0;
+                attempt = 0;
+                gamePoket = resetPoket;
+                attemptPrize = [maxPrize, midPrize, lowPrize];
+                confirm('Do you want play again?') ? playGame
+                    : attempt = maxAttempt + 1;
+                break;
+            case randomNumber !== playerNumber:
+                playerNumber = +prompt('Choose a roulette pocket number from 0 to ' + gamePoket +
+                    '\n Attempts left: ' + attemptLeft[attempt] +
+                    '\n Tottal prize: ' + prize +
+                    '$ \n Possible prize on current attempt ' + attemptPrize[attempt] + '$', '');
+                attempt++;
+                break;
+            default:
+                break;
+        }
+    }
+} else {
+    alert('You did not become a billionaire, but can.');
+}
+
+
