d7f800c45556aed4f8f2f123577ba275211e32b2
FyuroTEX <yurilev4@gmail.com>
2019-07-11 02:35:20 +0300

    formatTime

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -2,6 +2,6 @@ function formatTime(time) {
     let hour = (time / 60) % 24;
     let day = (time / 1440) % 60;
     let minuts = time % 60;
-    return Math.floor(day) + ' day(s) ' + hour.toFixed() + ' hour(s) ' + minuts + ' minute(s).';
+    return Math.floor(day) + ' day(s) ' + Math.floor(hour) + ' hour(s) ' + minuts + ' minute(s).';
 }
 console.log(formatTime(3601));
