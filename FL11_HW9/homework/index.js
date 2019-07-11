f65efa9200a8960b8620c7eb6bf2b19a259ea0c9
FyuroTEX <yurilev4@gmail.com>
2019-07-12 02:25:23 +0300

    hw9 0 task

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+function getNumbers(str) {
+    let arr = [];
+    arr += str.replace(/(\D)/gi, '');
+    return arr.split('');
+}
+console.log(getNumbers('строка'));
+console.log(getNumbers('n1um3ber95'));
