5b5280b94796e2bbdd47a440e6297349ceb81344
FyuroTEX <yurilev4@gmail.com>
2019-07-14 23:05:16 +0300

    hw9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -5,3 +5,18 @@ function getNumbers(str) {
 }
 console.log(getNumbers('строка'));
 console.log(getNumbers('n1um3ber95'));
+
+function findTypes(...item) {
+    let types = {};
+    item.forEach( i => {
+        if (types.hasOwnProperty(typeof i)) {
+            types[typeof i] += 1;
+        } else {
+            types[typeof i] = 1;
+        }
+
+    });
+    return types;
+}
+console.log(findTypes(null, 5, 'hello', 45, 'brom'));
+                  
